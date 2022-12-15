bd4ec0df4f150ee6d939f7024a24bc1709815b8c
BenP <ben.price2013@gmail.com>
2022-12-15 14:20:57 -0600

    Finish part one day one

--- Program.cs
+++ Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace AdventOfCode2022 {
+
+public class Elf {
+    public Elf(List<int> foods) {
+        Food = new List<int>(foods);
+    }
+    
+    public List<int> Food {get;}
+
+    public int TotalCalories() {
+        return Food.Sum();
+    }
+    
+    public int HighestCaloricFood() {
+        return Food.Aggregate((x, y) => x > y ? x : y);
+    }
+    
+    public int FoodCount() {
+        return Food.Count;
+    }
+}
+
+public class DayOne {
+    public static void Main() {
+        var elves = new List<Elf>();
+        var foods = new List<int>();
+
+        // grab our input file contents
+        string CD = AppDomain.CurrentDomain.BaseDirectory;
+        string inputFile = System.IO.Path.Combine(CD, @"..\..\..\inputs\01.txt");
+        string[] lines = System.IO.File.ReadAllLines(inputFile);
+        
+
+        // parse input file into our data structures
+        foreach (var line in lines) {
+            if (line.CompareTo("") == 0) {
+                elves.Add(new Elf(foods));
+                foods.Clear();
+            }
+            else {
+                foods.Add(Int32.Parse(line));
+            }
+        }
+        
+        Elf largestElf = elves.Aggregate((x, y) => x.TotalCalories() > y.TotalCalories() ? x : y);
+        Console.WriteLine(largestElf.TotalCalories());
+    }
+}
+
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Advent of Code 2022
+### Code by Benjamin Price
+
+Hello! This is the repository for my code for the Advent of Code 2022 competition.
