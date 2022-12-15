c4aeb7f834e14b389a12e8ee616a095f113eb8bb
BenP <ben.price2013@gmail.com>
2022-12-15 16:04:20 -0600

    Finish Day 1 Part 2

--- Program.cs
+++ Program.cs
@@ -3,13 +3,12 @@
 public class Elf {
     public Elf(List<int> foods) {
         Food = new List<int>(foods);
+        TotalCalories = Food.Sum();
     }
     
     public List<int> Food {get;}
 
-    public int TotalCalories() {
-        return Food.Sum();
-    }
+    public int TotalCalories {get;}
     
     public int HighestCaloricFood() {
         return Food.Aggregate((x, y) => x > y ? x : y);
@@ -42,8 +41,29 @@ public class DayOne {
             }
         }
         
-        Elf largestElf = elves.Aggregate((x, y) => x.TotalCalories() > y.TotalCalories() ? x : y);
-        Console.WriteLine(largestElf.TotalCalories());
+        Elf largestElf = elves.Aggregate((x, y) => x.TotalCalories > y.TotalCalories ? x : y);
+
+        Console.WriteLine("Top largest elf: " + largestElf.TotalCalories);        
+        
+        var topElves = new PriorityQueue<Elf, int>(4); // default PQ is a minimum PQ which is what we want
+        var iter = elves.GetEnumerator();
+
+        for (int i = 0; i < 3; i++) {
+            iter.MoveNext();
+            topElves.Enqueue(iter.Current, iter.Current.TotalCalories);
+        }
+        
+        while (iter.MoveNext()) {
+            if (iter.Current.TotalCalories > topElves.Peek().TotalCalories) {
+                topElves.EnqueueDequeue(iter.Current, iter.Current.TotalCalories);
+            }
+        } 
+        
+        int total = 0;
+        for (int i = 0; i < 3; i++) {
+            total += topElves.Dequeue().TotalCalories;
+        }
+        Console.WriteLine("Top 3 Largest Elves: " + total);
     }
 }
 
